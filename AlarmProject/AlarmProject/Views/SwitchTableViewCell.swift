f613c14ce419c881760d16ef08f5eeda785f7807
Blake Kvarfordt <blakekvarfordt@gmail.com>
2019-08-05 14:14:11 -0600

    Sets up StoryBoards / Adds outlets

---
+++ AlarmProject/AlarmProject/Views/SwitchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SwitchTableViewCell.swift
+//  AlarmProject
+//
+//  Created by Blake kvarfordt on 8/5/19.
+//  Copyright © 2019 Blake kvarfordt. All rights reserved.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
